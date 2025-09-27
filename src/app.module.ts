17c1a316cfb6a77694e2bcfd05b2fa87534e1dac
Juan P. Lima <contact@juanpireslima.com>
2025-09-27 16:54:45 -0500

    ci: using config module as a global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(ormConfig),
   ],
   controllers: [AppController],
