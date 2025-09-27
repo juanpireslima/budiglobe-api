9a5dd1a21c97e0e90016b54162738da146f52230
Juan P. Lima <contact@juanpireslima.com>
2025-09-27 16:43:58 -0500

    feat: initializing config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config as ormConfig } from '../ormconfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(ormConfig),
   ],
   controllers: [AppController],
